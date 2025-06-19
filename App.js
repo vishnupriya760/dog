94ae07a32f0de25e2864cffc10a619df58da7809
vishnupriya760 <vishnupriyaot7@gmail.com>
2025-06-19 10:13:19 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './component/Home';
+import Media from './component/Media';
+function App() {
+  return (
+    <div className='page'>
+
+      {/* <Media/> */}
+<Home />
+ 
+
+    </div>
+   
+  
+  
+  );
+}
+
+export default App;
---
+++ Home.js
@@ -0,0 +1,245 @@
+import React from 'react'
+import bowl from'../images/bowl.jpg'
+import beds from'../images/beds.jpg'
+import costumes from'../images/costumes.jpg'
+import toys from'../images/toys.jpg'
+import foods from'../images/foods.jpg'
+import cloth from'../images/cloth.jpg'
+import blue from'../images/blue.jpg'
+import yellow from'../images/yellow.jpg'
+import skyblue from'../images/sky blue.jpg'
+import green from'../images/green.jpg'
+import bl from'../images/bl.jpg'
+import cat from'../images/cat.jpg'
+import belt from'../images/belt.jpg'
+import birds from'../images/birds.jpg'
+import puppy from'../images/puppy.jpg'
+import meow from'../images/meow.jpg'
+import pink from'../images/pink.jpg'
+import sam from'../images/sam.jpg'
+import ultra from'../images/ultra.jpg'
+import tiki from'../images/tiki.jpg'
+
+
+
+
+const Home = () => {
+  return (
+    <div>
+      <div class="tot">
+        <div class="kok">
+        <div class="p">
+        < div class="p1">
+            <p>Demo</p>
+            <p>Shop</p>
+            <p>Products</p>
+            <p>Pages</p>
+            <p>LookBook</p>
+            </div>
+            <div class="d">
+            <p><b>Kalles</b></p>
+</div>
+            
+            
+        </div>
+        <div class="t">
+         <p class="t1">FREE SHIPPING</p>
+         <h1 class="head">Blanced Diet For You</h1>
+         <button class="b1">Shop Now</button>
+       </div>
+       </div>
+       </div>
+        <div class="box">
+
+          <div class="k">
+            <img src={bowl}class="im"></img>
+          </div>
+          <div class="k">
+            <img src={beds}class="im"></img>
+          </div>
+          <div class="k">
+              <img src={costumes}class="im"></img>
+
+
+          </div>
+         
+          <div class="k">
+             <img src={toys}class="im"></img>
+          </div>
+          <div class="k">
+            <img src={foods}class="im"></img>
+          </div>
+          <div class="k">
+            <img src={cloth}class="im"></img>
+          </div>
+</div>
+          <div class="text2">
+          <div class=" jk">Text</div>
+           <div class="j1">Beds</div>
+          <div class="j">Costumes</div>
+          <div class="kn">Toys</div>
+          <div class="n">Foods & pharmacy</div>
+          <div class="c">Clothing</div>
+          </div>
+          <div class="an">
+<div className='co'>
+  <img src ={blue}class="im2"></img>
+</div>
+<div className='co'>
+  <img src={yellow}class="im2"></img>
+</div>
+<div className='skyblue'>
+  <img src={skyblue}class="im2"></img>
+</div>
+<div className='green'>
+  <img src={green}class="im2"></img>
+</div>
+
+
+
+
+ 
+
+
+
+</div>
+
+ <h2 class="z"> New Arrival Products</h2>
+ <div className='zig'>
+ 
+ </div>
+ <div className='fi'>
+ <div className='bj'>
+  <img src={bl}class="im3"></img>
+
+ </div>
+ <div className='bj'>
+  <img src={cat}class="im4"></img>
+  
+ </div>
+ <div className='bj'>
+  <img src={belt}class="im5"></img>
+ </div>
+ <div className='bj'>
+  <img src={birds}class="im6"></img>
+ </div>
+ <div className='bj'>
+  <img src={puppy}class="im7"></img>
+ </div>
+ </div>
+ <div className='text3'>
+  <div className='e'>
+    <p >Dog's Standard Bowl 10'<br></br>
+    <p className='ll'>
+      $20.99
+      </p>
+    </p>
+  </div>
+  <div className='e1'>
+    <p>Cat Relaxed Blanket<br></br>
+    <p className='ll'>
+    $30.99
+      </p>
+    </p>
+  </div>
+   <div className='e2'>
+   <p> PetSafe Remote Training<br></br>
+   <p className='ll'>
+    $15.99
+   </p>
+   </p>
+   </div>
+   <div className='e3'>
+    <p>Wild Bird Food<br></br>
+    <p className='ll'>
+    $35.00
+    </p>
+    </p>
+   </div>
+   <div className='e4'>
+    <p>Pet Science Diet Food<br></br>
+     <p className='ll'> $32.99
+      </p>
+    </p>
+   </div>
+ </div>
+ <div className='mi'>
+ <div className='ko'>
+  <img src={meow}class="i2"></img>
+
+ </div>
+ <div className='ko'>
+  <img src={pink}class="i3"></img>
+  
+ </div>
+ <div className='ko'>
+  <img src={sam}class="i4"></img>
+ </div>
+ <div className='ko'>
+  <img src={ultra}class="i5"></img>
+ </div>
+ <div className='ko'>
+  <img src={tiki}class="i6"></img>
+ </div>
+</div>
+<div className='text4'>
+  <div className='a'>
+   <p> Giorgio Brato Washed Hobo<br></br>
+    <p className='ll'>$20.99</p>
+   </p>
+
+     </div>
+  <div className='a1'>
+    <p>Good Times Relaxed Shorts<br></br>
+    <p className='ll'>
+      $25.95
+      </p>
+      </p>
+    
+     </div>
+   <div className='a2'>
+   <p> Shag Vegan Donut Cuddler<br></br>
+   <p className='ll'>
+   $45.89
+   </p>
+    </p> 
+    </div>
+   <div className='a3'>
+    <p>The Supcrood Plate Ultra<br></br>
+    <p className='ll'>
+      Food<br></br>
+        $19.99
+        </p>
+    </p>
+     </div>
+   <div className='a4'> 
+<p>Tiki Pate For Cat<br></br>
+<p className='ll'>
+$35.95
+</p>
+</p>
+
+   </div>
+</div>
+</div>
+
+          
+
+        
+        
+        
+         
+        
+       
+
+       
+
+      
+        
+        
+        
+    
+  )
+}
+
+export default Home
